9359df35ff0bb01d2a6c82e06a46e1f99be4d7a3
cclauss <cclauss@bluewin.ch>
2016-02-21 17:51:53 +0100

    Going back to the original cloud_test.py

--- cloud_test.py
+++ cloud_test.py
@@ -1,26 +1,9 @@
 # coding: utf-8
 
-'''
-cloud.py
+import cloud
 
-Vision:
+cloud.Import('pythonista.editor')
+cloud.Import('Gestures')
 
-- cloud.Import: to make the entry curve to using code hosted on GitHub much easier
-
-Credits:
-
-- cloud.Import: idea and first version by @guerito, future versions on @webmaster4o's GitHub
-
-'''
-
-import cloud, os
-
-SITE_DIR = os.path.expanduser('~/Documents/site-packages')
-
-if __name__ == '__main__':
-	'''As a test of the system, try to add Gestures to site-packages'''
-	filename = 'Gestures'
-	filepath = os.path.join(SITE_DIR, filename)
-	print('Before: os.path.isfile({}) is {}'.format(filepath, os.path.isfile(filepath)))
-	cloud.Import(filename)
-	print(' After: os.path.isfile({}) is {}'.format(filepath, os.path.isfile(filepath)))
+pythonista.editor.WebTab().present()
+g = Gestures.Gestures()
