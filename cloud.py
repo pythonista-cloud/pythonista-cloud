29cff974e6bc662af71d943b4db013e353964645
cclauss <cclauss@bluewin.ch>
2016-02-22 22:36:00 +0100

    shorten long lines

--- cloud.py
+++ cloud.py
@@ -13,17 +13,23 @@ Credits:
 
 '''
 
-import bs4, urllib2, plistlib, shutil, zipfile, os, importlib, inspect, html2text, pickle, console
+import bs4, console, html2text, importlib, inspect, os, pickle, plistlib, shutil, urllib2, zipfile
+
+DOCS_DIR = os.path.expanduser('~/Documents')
+SITE_DIR = os.path.join(DOCS_DIR, 'site-packages')
+CLOUD_PKL = os.path.join(SITE_DIR, 'cloud.pkl')
+URL = 'http://forum.omz-software.com/topic/2775/cloud-import'
 
 def Import(sTarget):
-	for code in bs4.BeautifulSoup(urllib2.urlopen('http://forum.omz-software.com/topic/2775/cloud-import').read()).find_all('code'):
+	soup = bs4.BeautifulSoup(urllib2.urlopen(URL).read())
+	for code in soup.find_all('code', class_='xml'):
 		s = code.getText()
-		if s[:5] == '<?xml': 
+		if s.startswith('<?xml'): 
 			urlZ = plistlib.readPlistFromString(s)[sTarget]
 			break
 	d = dict()
-	if os.path.isfile(os.path.expanduser('~/Documents/site-packages/' + 'cloud.pkl')):
-		with open(os.path.expanduser('~/Documents/site-packages/' + 'cloud.pkl'), 'r') as f:
+	if os.path.isfile(CLOUD_PKL):
+		with open(CLOUD_PKL, 'r') as f:
 			d = pickle.Unpickler(f).load()
 	s = html2text.html2text(urllib2.urlopen(urlZ).read())
 	i = s.find('commits')
@@ -33,27 +39,28 @@ def Import(sTarget):
 	except:
 		iOld = -1
 	d[sTarget.split('.')[0]] = iNow
-	with open(os.path.expanduser('~/Documents/site-packages/' + 'cloud.pkl'), 'w') as f:
+	with open(CLOUD_PKL, 'w') as f:
 		pickle.Pickler(f).dump(d)
 	if iNow > iOld:
 		console.hud_alert('updating ' + sTarget + ' ...')
 		urlZ += '/archive/master.zip'
-		sZ = os.path.expanduser('~/Documents/'+  urlZ.split('/')[-1])
+		sZ = os.path.join(DOCS_DIR, urlZ.split('/')[-1])
 		shutil.copyfileobj(urllib2.urlopen(urlZ), open(sZ, 'wb'), length=512*1024)
 		with open(sZ, 'rb') as f:
 			for member in zipfile.ZipFile(f).namelist():
 				l = member.split('/')
 				if len(l) <= 2: # module
 					if l[-1][-3:] == '.py':
-						zipfile.ZipFile(f).extract(member, os.path.expanduser('~/Documents/'))
-						shutil.move(os.path.expanduser('~/Documents/'+ member), os.path.expanduser('~/Documents/site-packages/' + l[-1]))
+						zipfile.ZipFile(f).extract(member, DOCS_DIR)
+						shutil.move(os.path.join(DOCS_DIR, member), os.path.join(SITE_DIR, l[-1]))
 				else: # package
 					if l[1] == sTarget.split('.')[0] and l[-1] != '':
-						zipfile.ZipFile(f).extract(member, os.path.expanduser('~/Documents/'))
-						if not os.path.exists(os.path.expanduser('~/Documents/site-packages/' + l[-2])):
-							os.mkdir(os.path.expanduser('~/Documents/site-packages/' + l[-2]))
-						shutil.move(os.path.expanduser('~/Documents/'+ member), os.path.expanduser('~/Documents/site-packages/' + l[-2] + '/' + l[-1]))
-		shutil.rmtree(os.path.expanduser('~/Documents/' + l[0]))
+						zipfile.ZipFile(f).extract(member, DOCS_DIR)
+						dest_path = os.path.join(SITE_DIR, l[-2]))
+						if not dest_path:
+							os.mkdir(dest_path)
+						shutil.move(os.path.join(DOCS_DIR, member), os.path.join(dest_path, l[-1]))
+		shutil.rmtree(os.path.join(DOCS_DIR, l[0]))
 		os.remove(sZ)
 	locals()[sTarget.split('.')[0]] = importlib.import_module(sTarget.split('.')[0])
 	if len(sTarget.split('.')) != 1: locals()[sTarget.split('.')[1]] = importlib.import_module(sTarget)
