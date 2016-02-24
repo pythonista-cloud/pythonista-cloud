f0b9dc0526049ba2d087dc7911d1516b12eab125
cclauss <cclauss@bluewin.ch>
2016-02-24 01:25:08 +0100

    Create zip_file directly from URL

--- cloud.py
+++ cloud.py
@@ -13,7 +13,7 @@ Credits:
 
 '''
 
-import bs4, console, html2text, importlib, inspect, os, pickle, plistlib, shutil, urllib2, zipfile
+import bs4, console, cStringIO, html2text, importlib, inspect, os, pickle, plistlib, requests, shutil, zipfile
 
 DOCS_DIR = os.path.expanduser('~/Documents')
 SITE_DIR = os.path.join(DOCS_DIR, 'site-packages')
@@ -21,7 +21,7 @@ CLOUD_PKL = os.path.join(SITE_DIR, 'cloud.pkl')
 URL = 'http://forum.omz-software.com/topic/2775/cloud-import'
 
 def Import(sTarget):
-	soup = bs4.BeautifulSoup(urllib2.urlopen(URL).read())
+	soup = bs4.BeautifulSoup(requests.get(URL).text)
 	for code in soup.find_all('code', class_='xml'):
 		s = code.getText()
 		if s.startswith('<?xml'): 
@@ -31,7 +31,7 @@ def Import(sTarget):
 	if os.path.isfile(CLOUD_PKL):
 		with open(CLOUD_PKL, 'r') as f:
 			d = pickle.Unpickler(f).load()
-	s = html2text.html2text(urllib2.urlopen(urlZ).read())
+	s = html2text.html2text(requests.get(urlZ).text)
 	i = s.find('commits')
 	iNow = int(s[i - 4:i - 1])
 	try:
@@ -44,24 +44,22 @@ def Import(sTarget):
 	if iNow > iOld:
 		console.hud_alert('updating ' + sTarget + ' ...')
 		urlZ += '/archive/master.zip'
-		sZ = os.path.join(DOCS_DIR, urlZ.split('/')[-1])
-		shutil.copyfileobj(urllib2.urlopen(urlZ), open(sZ, 'wb'), length=512*1024)
-		with open(sZ, 'rb') as f:
-			for member in zipfile.ZipFile(f).namelist():
+		content = requests.get(urlZ, stream=True).content
+		with zipfile.ZipFile(cStringIO.StringIO(content)) as zip_file:
+			for member in zip_file.namelist():
 				l = member.split('/')
 				if len(l) <= 2: # module
 					if l[-1][-3:] == '.py':
-						zipfile.ZipFile(f).extract(member, DOCS_DIR)
+						zip_file.extract(member, DOCS_DIR)
 						shutil.move(os.path.join(DOCS_DIR, member), os.path.join(SITE_DIR, l[-1]))
 				else: # package
 					if l[1] == sTarget.split('.')[0] and l[-1] != '':
-						zipfile.ZipFile(f).extract(member, DOCS_DIR)
-						dest_path = os.path.join(SITE_DIR, l[-2]))
+						zip_file.extract(member, DOCS_DIR)
+						dest_path = os.path.join(SITE_DIR, l[-2])
 						if not dest_path:
 							os.mkdir(dest_path)
 						shutil.move(os.path.join(DOCS_DIR, member), os.path.join(dest_path, l[-1]))
 		shutil.rmtree(os.path.join(DOCS_DIR, l[0]))
-		os.remove(sZ)
 	locals()[sTarget.split('.')[0]] = importlib.import_module(sTarget.split('.')[0])
 	if len(sTarget.split('.')) != 1: locals()[sTarget.split('.')[1]] = importlib.import_module(sTarget)
 	reload(locals()[sTarget.split('.')[0]])
