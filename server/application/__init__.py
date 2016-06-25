e268345b9016c3c907a1cb81e48c9423472bc4f6
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-06-24 22:33:13 -0400

    Restructure a bit

--- server/application/__init__.py
+++ server/application/__init__.py
@@ -1,24 +1,30 @@
+""" The main pythonista.cloud application. Contains the basic logic for
+pythonista.cloud. """
+
 import os
 
 import flask
 
-
 localdir = os.path.abspath(os.path.dirname(__file__))
+staticdir = os.path.join(localdir, "../static")
+
 app = flask.Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return flask.send_file("site/main.html")
+    """ Return the main page (from /site/main.html) for requests to the file
+    root """
+    return flask.send_file(os.path.join(staticdir, "main.html"))
 
 
 @app.route("/<filepath>/")
 def returnFile(filepath):
     """ Attempt to return files from site from their equivalent path at the
     root (/main.html -> /site/main.html) """
-    testpath = os.path.join(localdir, "site/" + filepath)
+    testpath = os.path.join(staticdir, filepath)
     if os.path.exists(testpath):
-        return flask.send_from_directory("site", filepath)
+        return flask.send_from_directory(staticdir, filepath)
     else:
         flask.abort(404)
 
--- server/wsgi.py
+++ server/wsgi.py
@@ -1,4 +1,4 @@
-from main import app
+from application import app
 
 if __name__ == "__main__":
     app.run()
