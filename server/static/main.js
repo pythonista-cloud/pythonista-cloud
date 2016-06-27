43651120d564dd13b0aebb52ffb6a7d0dbb205e5
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-06-26 22:40:41 -0400

    Add syntax-highlighted example

--- server/static/main.js
+++ server/static/main.js
@@ -1,4 +1,19 @@
-particleground(document.getElementById("particles"), {
-  dotColor: '#99e0cf',
-  lineColor: '#99e0cf'
-});
+window.onload = function () {
+  // Initialize particleground
+  particleground(document.getElementById("particles"), {
+    dotColor: '#99e0cf',
+    lineColor: '#99e0cf'
+  });
+  // Little snippet I wrote to magic-ify whitespace in pre > code blocks.
+  // Removes leading and trailing newlines, and normalizes indentation.
+  toStrip = document.getElementsByClassName("magic-indent");
+  for (var i = 0; i < toStrip.length; i++) {
+    var elem = toStrip[i].getElementsByTagName("code")[0];
+    var lines = elem.innerHTML.replace(/^\n+|\n+$/g,"").split("\n");
+    var ilevel = lines[0].match(/^\s*/)[0].length;
+    var out = "";
+    for (var k = 0; k < lines.length; k++)
+      out += lines[k].slice(ilevel) + (k < lines.length-1 ? "\n":"");
+    elem.innerHTML = out;
+  }
+};
