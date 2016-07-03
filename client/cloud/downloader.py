bfaa274eb63c40b74226aa10ca46923956ba04f7
Luke Deen Taylor <ldt@deentaylor.com>
2016-07-03 17:05:47 -0400

    Add DownloadedModule class

--- client/cloud/downloader.py
+++ client/cloud/downloader.py
@@ -5,8 +5,18 @@ zip files via GitHub.
 """
 
 
+class DownloadedModule(object):
+    """A class to store basic information about a downloaded module.
+    
+    Stores the path to a downloaded zip file, as well as metadata like GitHub
+    URL and supported Python versions.
+    """
+    def __init__(self, zip_path, metadata):
+        self.zip_path = zip_path
+        for i in metadata:
+            setattr(self, i, metadata[i])
+
 def download(module_name):
     """Download a zip archive for a remote module."""
-    raise NotImplementedError()
-    # Something...
+    
     return DownloadedModule(extracted_path, metadata)
